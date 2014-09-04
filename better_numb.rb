36ae71a59599f775ce0ed343bfd53c78d05d5242
Allie Sterling <sterlina89@gmail.com>
2014-09-03 21:13:05 -0700

    First Commit

---
+++ better_numb.rb
@@ -0,0 +1,9 @@
+puts "Quick question: What's your favorite number OF ALL TIME?"
+response = gets.chomp.to_i
+plus_one = response + 1
+
+
+puts "May I suggest an EVEN BETTER NUMBER?"
+puts plus_one.to_s + " is a way better number!"
+
+puts "You are welcome!"
