27be6adf478a2d2ebc7a2dd8567155dc9fe5cf03
Allie Sterling <sterlina89@gmail.com>
2014-09-03 21:16:01 -0700

    First commit for greeting

---
+++ greeting.rb
@@ -0,0 +1,13 @@
+puts "Hey!  What is your first name?"
+
+first = gets.chomp
+
+puts "Okay, great.  How about your middle name?"
+
+middle = gets.chomp
+
+puts "And for the grand finale, what about your last name?"
+
+last = gets.chomp
+
+puts "Lovely to meet you, " + first + ' ' + middle + ' ' + last + "!"
